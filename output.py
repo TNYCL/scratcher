d6eeba6647b5a29c4abf0148d3330d05cb03af6f
TNYCL <73045039+TNYCL@users.noreply.github.com>
2021-09-26 12:12:15 +0300

    gg

--- output.py
+++ output.py
@@ -3,17 +3,17 @@ import os
 import input
 
 
-def duplicate(project_name, author):
+def duplicateFolder(project_name, author):
     pname = input.project_name
-
     src = "project_template"
     dest = "projects/" + pname
     behavior = dest + "/" + pname + " BP"
     resource = dest + "/" + pname + " RP"
-
     try:
         shutil.copytree(src, dest)
         os.rename(dest + "/BP", behavior)
         os.rename(dest + "/RP", resource)
     except FileExistsError as error:
         print("This project template already created.")
+
+
